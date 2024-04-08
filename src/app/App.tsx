e24ba4d3f8bcba223c0804f800269c2a53cdb9d3
Belkawwwwwww <alnblkrv@gmail.com>
2024-04-08 21:43:02 +0500

    3. Создать global.scss : добавлены шрифты(возможно изменю), normalize.css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import type {AppProps} from 'next/app'
 import {Providers} from "@/app/providers/providers";
+import './styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps & { Component: { layout: any }}) {
     const Layout = Component.layout || (({ children } : { children: React.ReactNode }) => <>{children}</>);
