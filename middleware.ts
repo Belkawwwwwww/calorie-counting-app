35f7fbee105cefb5f0c67dbc9f1671c95d1e2f07
Алена <alnblkrv@gmail.com>
2024-11-26 16:01:39 +0500

    удален неиспользуемый код

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,17 @@
-import {NextRequest, NextResponse} from 'next/server';
-import {isAuthSelector} from '@/f - entities/redux/session';
-import {useAppSelector} from '@/g - shared/lib/store';
+import { NextRequest, NextResponse } from 'next/server';
+import { isAuthSelector } from '@/f - entities/redux/session';
+import { useAppSelector } from '@/g - shared/lib/store';
 
 const protectedRoutes = ['/main', '/profile'];
 
 export function middleware(req: NextRequest, isAuth: boolean) {
-    // const isAuth = useAppSelector(isAuthSelector);
     if (!isAuth && protectedRoutes.includes(req.nextUrl.pathname)) {
         const absoluteURL = new URL('/', req.nextUrl.origin);
         return NextResponse.redirect(absoluteURL.toString());
     }
-
-    // const {pathname} = req.nextUrl
-    // if (isAuth && pathname === '/login') {
-    //     return NextResponse.redirect(new URL('/main', req.url))
-    // }
-    // if (!isAuth && pathname !== '/login') {
-    //     return NextResponse.redirect(new URL('/login', req.url))
-    // }
 }
 
 export default function (req: NextRequest) {
     const isAuth = useAppSelector(isAuthSelector);
     return middleware(req, isAuth);
 }
-
-// export const config = {
-//     matcher: '/((?!api|static|.*\\..*|_next).*)',
-// };
--- src/f - entities/redux/user/index.ts
+++ src/f - entities/redux/user/index.ts
@@ -1,3 +1,2 @@
 export {userSlice} from './model/slice/userSlice'
-// export type {UserObject} from './model/type/user'
 export {userDataSelector} from './model/selector/user'
